3c7e383a8bb414ede48827039bdac9d0f4a07147
John Nguyen <jnguyen@thefrontiergroup.com.au>
2017-08-23 21:49:42 +0800

    initial commit

---
+++ Dockerfile
@@ -0,0 +1,1 @@
+from 2.7-alpine
---
+++ run.py
@@ -0,0 +1,11 @@
+from chatterbot import ChatBot
+
+chatbot = ChatBot(
+            'Ron Obvious',
+                trainer='chatterbot.trainers.ChatterBotCorpusTrainer'
+                )
+
+# Train based on the english corpus
+#chatbot.train("chatterbot.corpus.english")
+
+print os.args
